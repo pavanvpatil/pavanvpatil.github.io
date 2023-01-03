02cee849ba50fe8be0781425b12b33365aed315b
pavanvpatil <pavanvpatil01@gmail.com>
2023-01-03 19:28:05 +0530

    Updates

---
+++ precache-manifest.fc4f56561cb453306f4e4e90bc29027f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e14059337b7c4b7525f",
+    "url": "/static/css/main.2a3f9b2a.chunk.css"
+  },
+  {
+    "revision": "6e14059337b7c4b7525f",
+    "url": "/static/js/main.6e140593.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "df31ad1c9a5c37b94e08b221c3c9d8a5",
+    "url": "/index.html"
+  }
+];
